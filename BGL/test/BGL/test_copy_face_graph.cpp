904200b14861952af76f7ecffc73cc4dd46d51ae
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-04-20 20:37:48 +0200

    Add a test for copy_face_graph

---
+++ BGL/test/BGL/test_copy_face_graph.cpp
@@ -0,0 +1,14 @@
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/boost/graph/copy_face_graph.h>
+
+typedef CGAL::Simple_cartesian<double> K;
+typedef CGAL::Polyhedron_3<K> Polyhedron;
+
+int main()
+{
+  Polyhedron A, B;
+
+  CGAL::copy_face_graph(A,B);
+  return 0;
+}
