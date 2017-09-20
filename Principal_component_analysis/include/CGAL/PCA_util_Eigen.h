4378e091cbbb80b46072f5b7cd7dad7d2b944d32
Andreas Fabri <andreas.fabri@geometryfactory.com>
2017-09-20 10:18:29 +0100

    Follow recommandation of Gael Guennebaud for matrix multiplication

--- Principal_component_analysis/include/CGAL/PCA_util_Eigen.h
+++ Principal_component_analysis/include/CGAL/PCA_util_Eigen.h
@@ -84,15 +84,16 @@ assemble_covariance_matrix_3(InputIterator first,
     // Find the 2nd order moment for the triangle wrt to the origin by an affine transformation.
     
     // Transform the standard 2nd order moment using the transformation matrix
-    transformation = transformation * moment * transformation.transpose();
+    Matrix mt = moment * transformation.transpose();
+    Matrix tmt =  transformation * mt;
     
     // and add to covariance matrix
-    covariance[0] += 2 * area * transformation(0, 0);
-    covariance[1] += 2 * area * transformation(1, 0);
-    covariance[2] += 2 * area * transformation(2, 0);
-    covariance[3] += 2 * area * transformation(1, 1);
-    covariance[4] += 2 * area * transformation(2, 1);
-    covariance[5] += 2 * area * transformation(2, 2);
+    covariance[0] += 2 * area * tmt(0, 0);
+    covariance[1] += 2 * area * tmt(1, 0);
+    covariance[2] += 2 * area * tmt(2, 0);
+    covariance[3] += 2 * area * tmt(1, 1);
+    covariance[4] += 2 * area * tmt(2, 1);
+    covariance[5] += 2 * area * tmt(2, 2);
 
     mass += area;
   }
@@ -660,7 +661,7 @@ assemble_covariance_matrix_2(InputIterator first,
     FT xav0 = (transformation(0,0) + transformation(0,1)) / 3.0;
     FT yav0 = (transformation(1,0) + transformation(1,1)) / 3.0;
       
-    FT area = 0.5 * std::abs(transformation.determinant());
+    FT area = 0.5 * CGAL::abs(transformation.determinant());
 
 		// skip zero measure primitives
     if(area == (FT)0.0)
@@ -669,12 +670,12 @@ assemble_covariance_matrix_2(InputIterator first,
     // Find the 2nd order moment for the triangle wrt to the origin by an affine transformation.
     
     // Transform the standard 2nd order moment using the transformation matrix
-    transformation =  transformation * moment * transformation.transpose();
-    
+    Matrix mt = moment * transformation.transpose();
+    Matrix tmt =  transformation * mt;
     // and add to covariance matrix
-    covariance[0] += 2 * area * transformation(0, 0) + area * (t[0].x() * xav0 * 2 + t[0].x() * t[0].x());
-    covariance[1] += 2 * area * transformation(1, 0) + area * (t[0].x() * yav0 + xav0 * t[0].y() + t[0].x() * t[0].y());
-    covariance[2] += 2 * area * transformation(1, 1) + area * (t[0].y() * yav0 * 2 + t[0].y() * t[0].y());
+    covariance[0] += 2 * area * tmt(0, 0) + area * (t[0].x() * xav0 * 2 + t[0].x() * t[0].x());
+    covariance[1] += 2 * area * tmt(1, 0) + area * (t[0].x() * yav0 + xav0 * t[0].y() + t[0].x() * t[0].y());
+    covariance[2] += 2 * area * tmt(1, 1) + area * (t[0].y() * yav0 * 2 + t[0].y() * t[0].y());
 
     mass += area;
   }
