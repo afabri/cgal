8459095053157c28088ec2273bfc52148b0ff5a8
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-12-14 17:37:06 +0100

    Improved SMS parallel examples

--- Surface_mesh_simplification/examples/Surface_mesh_simplification/parallel_edge_collapse.cpp
+++ Surface_mesh_simplification/examples/Surface_mesh_simplification/parallel_edge_collapse.cpp
@@ -19,86 +19,69 @@
 typedef CGAL::Simple_cartesian<double>                                      Kernel;
 
 typedef Kernel::Point_3                                                     Point_3;
-typedef CGAL::Surface_mesh<Point_3>                                         Surface_mesh;
-
-typedef std::vector<boost::graph_traits<Surface_mesh>::vertices_size_type>  Polygon_3;
+typedef CGAL::Surface_mesh<Point_3>                                         Triangle_mesh;
 
 namespace SMS = CGAL::Surface_mesh_simplification;
 namespace PMP = CGAL::Polygon_mesh_processing;
 
-// Usage: ./parallel_edge_collapse input_mesh.ply keep_ratio (default:0.25) number_of_tasks (default: 8)
+// Usage: ./parallel_edge_collapse input_mesh keep_ratio (default:0.25) number_of_tasks (default: 8)
 
 int main(int argc, char** argv)
 {
-  typedef boost::graph_traits<Surface_mesh>::edge_descriptor edge_descriptor;
-  typedef boost::graph_traits<Surface_mesh>::face_descriptor face_descriptor;
+  typedef boost::graph_traits<Triangle_mesh>::edge_descriptor edge_descriptor;
+  typedef boost::graph_traits<Triangle_mesh>::face_descriptor face_descriptor;
+
+  std::ifstream in((argc>1) ? argv[1] : "data/elephant.off");
+  double ratio = (argc>2) ? boost::lexical_cast<double>(argv[2]) : 0.25;
+  int number_of_parts = (argc>3) ? boost::lexical_cast<int>(argv[3]) : 8;
 
   CGAL::Real_timer t;
   t.start();
 
-  Surface_mesh sm;
+  Triangle_mesh tm;
+  in >> tm;
 
-  std::ifstream in((argc>1) ? argv[1] : "data/elephant.ply", std::ios_base::binary);
 
-  std::vector<Point_3> points;
-  std::vector<Polygon_3> polygons;
-  CGAL::read_PLY(in, points, polygons);
-  CGAL::Polygon_mesh_processing::polygon_soup_to_polygon_mesh (points, polygons, sm);
-
-  std::cerr << "Input: #V = "<< num_vertices(sm) << " #E = "<< num_edges(sm)
-            << " #F = " << num_faces(sm) << " read in " << t.time() << " sec." << std::endl;
+  std::cerr << "Input: #V = "<< num_vertices(tm) << " #E = "<< num_edges(tm)
+            << " #F = " << num_faces(tm) << " read in " << t.time() << " sec." << std::endl;
   t.reset();
-  double ratio = (argc>2) ? boost::lexical_cast<double>(argv[2]) : 0.25;
 
-  Surface_mesh::Property_map<face_descriptor,std::size_t> fpmap
-    = sm.add_property_map<face_descriptor,std::size_t>("f:partition").first;
+  // Partition ID map
+  Triangle_mesh::Property_map<face_descriptor, std::size_t> fpmap
+    = tm.add_property_map<face_descriptor, std::size_t>("f:partition").first;
 
-  Surface_mesh::Property_map<edge_descriptor,bool> ecmap
-    = sm.add_property_map<edge_descriptor,bool>("e:ec", false).first;
+  PMP::partition(tm, number_of_parts, fpmap);
+  std::cerr << "Built partition in " << t.time() << " sec."<< std::endl;
+  t.reset();
 
-  ecmap[*(edges(sm).first)] = true;
+  // Constrained edges map
+  Triangle_mesh::Property_map<edge_descriptor, bool> ecmap
+    = tm.add_property_map<edge_descriptor, bool>("e:ec", false).first;
 
-  int partition_size = (argc>3) ? boost::lexical_cast<int>(argv[3]) : 8;
+  ecmap[*(edges(tm).first)] = true;
 
-  if(partition_size > 1) {
-    PMP::partition(sm, fpmap, partition_size);
+  // Simplify the mesh
+  SMS::Bounded_normal_change_placement<SMS::LindstromTurk_placement<Triangle_mesh> > placement;
+  SMS::LindstromTurk_cost<Triangle_mesh> cost;
+  SMS::Count_ratio_stop_predicate<Triangle_mesh> stop(ratio);
 
-    std::cerr << "Built partition in " << t.time() << " sec."<< std::endl;
-    t.reset();
-  }
-  SMS::Bounded_normal_change_placement<SMS::LindstromTurk_placement<Surface_mesh> > placement;
-  SMS::LindstromTurk_cost<Surface_mesh> cost;
-  SMS::Count_ratio_stop_predicate<Surface_mesh> stop(ratio);
-
-  if(partition_size > 1)
-  {
-    SMS::parallel_edge_collapse(sm, stop, fpmap, partition_size,
-                                CGAL::parameters::get_placement(placement)
-                                                 .edge_is_constrained_map(ecmap)
-                                                 .get_cost(cost));
-  }
-  else
-  {
-    SMS::edge_collapse(sm, stop,
-                       CGAL::parameters::get_placement(placement)
-                                        .edge_is_constrained_map(ecmap)
-                                        .get_cost(cost));
-  }
+  SMS::parallel_edge_collapse(tm, stop, fpmap, number_of_parts,
+                              CGAL::parameters::edge_is_constrained_map(ecmap)
+                              .get_placement(placement)
+                              .get_cost(cost));
 
   // Specific to the class Surface_mesh, to clean deleted elements
-  sm.collect_garbage();
+  tm.collect_garbage();
 
   std::cerr << "\nSimplify in " << t.time() << " sec.\n"
-            << "Result: #V = " << num_vertices(sm)
-            << " #E = " << num_edges(sm)
-            << " #F = " << num_faces(sm) << std::endl;
+            << "Result: #V = " << num_vertices(tm)
+            << " #E = " << num_edges(tm)
+            << " #F = " << num_faces(tm) << std::endl;
 
   t.reset();
 
-  std::ofstream out("out.ply", std::ios_base::binary);
-  CGAL::set_binary_mode(out);
-  CGAL::write_PLY(out, sm);
-  out.close();
+  std::ofstream out("simplified_mesh.off");
+  out << tm;
   std::cerr << "Writing result in " << t.time() << " sec." << std::endl;
 
   return EXIT_SUCCESS;
--- Surface_mesh_simplification/examples/Surface_mesh_simplification/parallel_edge_collapse_Polyhedron.cpp
+++ Surface_mesh_simplification/examples/Surface_mesh_simplification/parallel_edge_collapse_Polyhedron.cpp
@@ -21,9 +21,7 @@
 typedef CGAL::Simple_cartesian<double>                                      Kernel;
 
 typedef Kernel::Point_3                                                     Point_3;
-typedef CGAL::Polyhedron_3<Kernel,CGAL::Polyhedron_items_with_id_3>         Surface_mesh;
-
-typedef std::vector<boost::graph_traits<Surface_mesh>::vertices_size_type>  Polygon_3;
+typedef CGAL::Polyhedron_3<Kernel, CGAL::Polyhedron_items_with_id_3>        Triangle_mesh;
 
 namespace SMS = CGAL::Surface_mesh_simplification;
 namespace PMP = CGAL::Polygon_mesh_processing;
@@ -32,89 +30,64 @@ namespace PMP = CGAL::Polygon_mesh_processing;
 
 int main(int argc, char** argv )
 {
-  typedef boost::graph_traits<Surface_mesh>::vertex_descriptor    vertex_descriptor;
-  typedef boost::graph_traits<Surface_mesh>::edge_descriptor      edge_descriptor;
-  typedef boost::graph_traits<Surface_mesh>::halfedge_descriptor  halfedge_descriptor;
-  typedef boost::graph_traits<Surface_mesh>::face_descriptor      face_descriptor;
+  typedef boost::graph_traits<Triangle_mesh>::edge_descriptor      edge_descriptor;
+  typedef boost::graph_traits<Triangle_mesh>::face_descriptor      face_descriptor;
+
+  std::ifstream in((argc>1) ? argv[1] : "data/elephant.off");
+  double ratio = (argc>2) ? boost::lexical_cast<double>(argv[2]) : 0.25;
+  int ncc = (argc>3) ? boost::lexical_cast<int>(argv[3]) : 8;
+
+  if(!in)
+  {
+    std::cerr << "Error: could not open input mesh" << std::endl;
+    return EXIT_FAILURE;
+  }
 
   CGAL::Real_timer t;
   t.start();
 
-  Surface_mesh sm;
-  std::ifstream in((argc>1) ? argv[1] : "data/elephant.ply", std::ios_base::binary);
+  Triangle_mesh tm;
+  in >> tm;
 
-  std::vector<Point_3> points;
-  std::vector<Polygon_3> polygons;
-  CGAL::read_PLY(in, points, polygons);
-  CGAL::Polygon_mesh_processing::polygon_soup_to_polygon_mesh (points, polygons, sm);
+  CGAL::set_halfedgeds_items_id(pm);
 
-  int index = 0;
-
-  BOOST_FOREACH(halfedge_descriptor hd, halfedges(sm)) {
-    hd->id() = index++;
-  }
-
-  index = 0 ;
-  BOOST_FOREACH(vertex_descriptor vd, vertices(sm)) {
-    vd->id() = index++;
-  }
-
-  std::cerr << "Input: #V = "<< num_vertices(sm)  << " #E = "<< num_edges(sm)
-            << " #F = " << num_faces(sm)  <<  " read in " << t.time() << " sec." << std::endl;
+  std::cerr << "Input: #V = "<< num_vertices(tm) << " #E = "<< num_edges(tm)
+            << " #F = " << num_faces(tm) << " read in " << t.time() << " sec." << std::endl;
   t.reset();
-  double ratio = (argc>2) ? boost::lexical_cast<double>(argv[2]) : 0.25;
 
-  std::map<face_descriptor,std::size_t> ccmap;
-  boost::associative_property_map<std::map<face_descriptor,std::size_t> > ccpmap(ccmap);
+  std::map<face_descriptor, std::size_t> ccmap;
+  boost::associative_property_map<std::map<face_descriptor, std::size_t> > ccpmap(ccmap);
 
-  std::map<edge_descriptor,bool> ecmap;
+  PMP::partition(tm, ncc /*number of partitions*/, ccpmap);
+  std::cerr << "Built partition in " << t.time() << " sec." << std::endl;
+  t.reset();
+
+  std::map<edge_descriptor, bool> ecmap;
   boost::associative_property_map<std::map<edge_descriptor, bool> > ecpmap(ecmap);
-  BOOST_FOREACH(edge_descriptor ed, edges(sm)){
+  BOOST_FOREACH(edge_descriptor ed, edges(tm)) {
     ecmap[ed] = false;
   }
 
-  ecmap[*(edges(sm).first)] = true;
+  ecmap[*(edges(tm).first)] = true; // arbitrarily constrain the first edge
 
-  int ncc = (argc>3) ? boost::lexical_cast<int>(argv[3]) : 8;
+  SMS::Bounded_normal_change_placement<SMS::LindstromTurk_placement<Triangle_mesh> > placement;
+  SMS::LindstromTurk_cost<Triangle_mesh> cost;
+  SMS::Count_ratio_stop_predicate<Triangle_mesh> stop(ratio);
 
-  if(ncc > 1) {
-    PMP::partition(sm, ccpmap, ncc);
-
-    std::cerr << "Built partition in " << t.time() << " sec." << std::endl;
-    t.reset();
-  }
-
-  SMS::Bounded_normal_change_placement<SMS::LindstromTurk_placement<Surface_mesh> > placement;
-  SMS::LindstromTurk_cost<Surface_mesh> cost;
-  SMS::Count_ratio_stop_predicate<Surface_mesh> stop(ratio);
-
-  if(ncc > 1)
-  {
-    SMS::parallel_edge_collapse(sm, stop, ccpmap, ncc,
-                                CGAL::parameters::get_placement(placement)
-                                  .edge_is_constrained_map(ecpmap)
-                                  .get_cost(cost));
-  }
-  else
-  {
-    SMS::edge_collapse(sm, stop,
-                       CGAL::parameters::get_placement(placement)
-                         .edge_is_constrained_map(ecpmap)
-                         .get_cost(cost));
-  }
+  SMS::parallel_edge_collapse(tm, stop, ccpmap, ncc,
+                              CGAL::parameters::edge_is_constrained_map(ecpmap)
+                              .get_placement(placement)
+                              .get_cost(cost));
 
   std::cerr << "\nSimplify in " << t.time() << " sec.\n"
-            << "Result: #V = " << num_vertices(sm)
-            << " #E = " << num_edges(sm)
-            << " #F = " << num_faces(sm) << std::endl;
+            << "Result: #V = " << num_vertices(tm)
+            << " #E = " << num_edges(tm)
+            << " #F = " << num_faces(tm) << std::endl;
 
   t.reset();
 
-  std::ofstream out("outP.ply", std::ios_base::binary);
-  CGAL::set_binary_mode(out);
-  CGAL::write_PLY(out, sm);
-  out.close();
-
+  std::ofstream out("simplified_mesh.off");
+  out << tm;
   std::cerr << "Writing result in " << t.time() << " sec." << std::endl;
 
   return EXIT_SUCCESS;
