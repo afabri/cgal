de5419e9cb28813cf42d3f1a984ec5eabde121e8
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2018-01-08 11:35:48 +0100

    Minor partition example improvements

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/partition_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/partition_example.cpp
@@ -9,9 +9,11 @@ namespace PMP = CGAL::Polygon_mesh_processing;
 
 int main(int argc, char** argv)
 {
-  typedef CGAL::Simple_cartesian<double>                             K;
+  typedef CGAL::Simple_cartesian<double>                           K;
 
   std::ifstream in((argc>1) ? argv[1] : "data/mech-holes-shark.off");
+  int number_of_parts = (argc>2) ? atoi(argv[2]) : 8;
+
   if(!in)
   {
     std::cerr << "Error: could not read input file" << std::endl;
@@ -26,18 +28,17 @@ int main(int argc, char** argv)
   typedef SM::Property_map<SM::Face_index, std::size_t>            Face_id_map;
   Face_id_map partition_id_map = sm.add_property_map<SM::Face_index, std::size_t>("f:pid").first;
 
-  // Partition the mesh and output its parts
-  PMP::partition(sm, 8 /*number of partitions*/, partition_id_map);
+  // Partition the mesh
+  PMP::partition(sm, number_of_parts, partition_id_map);
 
-  // Extract the part n°4 of the partition into a new, independent mesh
-  typedef CGAL::Face_filtered_graph<SM>                       Filtered_graph;
-  Filtered_graph filtered_sm(sm, 4 /*id of th part*/, partition_id_map);
+  // Extract the part n°0 of the partition into a new, independent mesh
+  typedef CGAL::Face_filtered_graph<SM>                            Filtered_graph;
+  Filtered_graph filtered_sm(sm, 0 /*id of th part*/, partition_id_map);
   SM part_sm;
   CGAL::copy_face_graph(filtered_sm, part_sm);
 
-  std::ofstream out("sm_part_4.off");
+  std::ofstream out("sm_part_0.off");
   CGAL::write_off(out, part_sm);
 
   return EXIT_SUCCESS;
 }
-
--- Polygon_mesh_processing/examples/Polygon_mesh_processing/partition_example_adv.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/partition_example_adv.cpp
@@ -7,21 +7,23 @@ namespace PMP = CGAL::Polygon_mesh_processing;
 
 int main(int argc, char** argv)
 {
-  typedef CGAL::Simple_cartesian<double>                             K;
+  typedef CGAL::Simple_cartesian<double>                            K;
 
   std::ifstream in((argc>1) ? argv[1] : "data/mech-holes-shark.off");
+  int number_of_parts = (argc>2) ? atoi(argv[2]) : 8;
+
   if(!in)
   {
     std::cerr << "Error: could not read input file" << std::endl;
     return EXIT_FAILURE;
   }
 
-  typedef CGAL::Polyhedron_3<K>  PM;
+  typedef CGAL::Polyhedron_3<K>                                     PM;
   PM pm;
   in >> pm; // read the mesh
 
-  // The face <--> partition_id property map  
-  typedef CGAL::dynamic_face_property_t<int> Face_property_tag;
+  // The face <--> partition_id property map
+  typedef CGAL::dynamic_face_property_t<int>                        Face_property_tag;
   typedef typename boost::property_map<PM, Face_property_tag>::type Face_id_map;
   Face_id_map partition_id_map = get(Face_property_tag(), pm);
 
@@ -38,16 +40,15 @@ int main(int argc, char** argv)
   options[METIS_OPTION_NCUTS] = 3;
   options[METIS_OPTION_NITER] = 10;
   options[METIS_OPTION_SEED] = 12345;
-  options[METIS_OPTION_MINCONN] = 0;
-  options[METIS_OPTION_CONTIG] = 0;
-  options[METIS_OPTION_UFACTOR] = 1;
+  options[METIS_OPTION_MINCONN] = 1;
+  options[METIS_OPTION_CONTIG] = 1;
+  options[METIS_OPTION_UFACTOR] = 25;
 
   // Partition the mesh and output its parts
-  PMP::partition(pm, 8 /*number of partitions*/,
+  PMP::partition(pm, number_of_parts,
                  partition_id_map,
                  CGAL::parameters::METIS_options(&options).
-                 vertex_index_map(get(boost::vertex_external_index, pm)) );
+                 vertex_index_map(get(boost::vertex_external_index, pm)));
 
   return EXIT_SUCCESS;
 }
-
