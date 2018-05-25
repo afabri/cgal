7785be741cdd7ba4fcf64e1484c86df573f33432
Andreas Fabri <andreas.fabri@geometryfactory.com>
2018-05-25 08:47:43 +0100

    Write a wrapper

--- BGL/examples/BGL_surface_mesh/round_edges_SM.cpp
+++ BGL/examples/BGL_surface_mesh/round_edges_SM.cpp
@@ -3,6 +3,7 @@
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/boost/graph/Euler_operations.h>
 #include <CGAL/boost/graph/helpers.h>
+#include <CGAL/Polygon_mesh_processing/compute_normal.h>
 
 #include <iostream>
 #include <fstream>
@@ -20,12 +21,45 @@ typedef boost::graph_traits<Mesh>::edge_descriptor           edge_descriptor;
 typedef boost::graph_traits<Mesh>::face_descriptor           face_descriptor;
 
 
+template <typename TM, typename V>
+std::vector<std::pair<typename boost::graph_traits<TM>::vertex_descriptor,V> >
+round(TM& tm, const std::vector<typename boost::graph_traits<TM>::halfedge_descriptor>& hedges)
+{
+  typedef boost::graph_traits<TM>::halfedge_descriptor halfedge_descriptor;
+  typedef boost::graph_traits<TM>::face_descriptor     face_descriptor;
+  
+  assert(tm.is_valid());
+  assert(CGAL::is_triangle_mesh(tm));
+  
+  std::vector<std::pair<typename boost::graph_traits<TM>::vertex_descriptor,V> > res(2*(hedges.size()-1));
+  
+  for(int i = 0; i < hedges.size()-1; ++i){
+    V v = CGAL::Polygon_mesh_processing::compute_face_normal(face(hedges[i],tm),tm);
+    v = v/(v*v);
+    res[i] = std::make_pair(target(hedges[i],tm), v);
+  }
+  CGAL::Euler::round_edges(hedges,tm);
+  std::size_t offset = hedges.size() - 1;
+  for(int i = 0; i < offset; ++i){
+    halfedge_descriptor hd = hedges[i];
+    hd = prev(opposite(hd,tm),tm);
+    hd = next(opposite(hd,tm),tm);
+    hd = opposite(hd,tm);
+    V v = CGAL::Polygon_mesh_processing::compute_face_normal(face(hd,tm),tm);
+    v = v/(v*v);
+    res[i+offset] = std::make_pair(target(hd,tm),v);
+  }
+  
+  return res;
+}
+
+
 int main(int argc, char* argv[])
 {
-  Mesh sm;
+  Mesh tm;
 
   std::ifstream inm(argv[1]);
-  inm >> sm;
+  inm >> tm;
   
   std::ifstream ins(argv[2]);
   int i,j;
@@ -33,20 +67,16 @@ int main(int argc, char* argv[])
 
   while(ins >> i >> j){
     vertex_descriptor vi(i), vj(j);
-    std::pair<edge_descriptor,bool> edb = edge(vi,vj,sm);
+    std::pair<edge_descriptor,bool> edb = edge(vi,vj,tm);
     assert(edb.second);
-    halfedge_descriptor hd = halfedge(edb.first,sm);
-    assert(target(hd,sm) == vj);
+    halfedge_descriptor hd = halfedge(edb.first,tm);
+    assert(target(hd,tm) == vj);
     hedges.push_back(hd);
   }
-  assert(sm.is_valid());
-  assert(CGAL::is_triangle_mesh(sm));
-  std::cout << num_vertices(sm) << std::endl;
-  CGAL::Euler::round_edges(hedges,sm);
 
-  std::cout << num_vertices(sm) << std::endl;
-    
-  assert(sm.is_valid());
+  std::vector<std::pair<boost::graph_traits<Mesh>::vertex_descriptor,Kernel::Vector_3> >  vvpairs;
+  vvpairs = round<Mesh,Kernel::Vector_3>(tm, hedges);
+  assert(tm.is_valid());
   std::cout << "done" << std::endl;
   return 0;
 }
