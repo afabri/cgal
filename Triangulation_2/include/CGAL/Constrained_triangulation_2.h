afabri/cgal

commit ecfd82e287378977a325ae34e21087a2075a05cd

Fix an infinite loop in CDT<EPIC, TDS, Exact_predicates_tag> Add a test, that know passes, and previously failed.

Triangulation_2/include/CGAL/Constrained_triangulation_2.h
@@ -681,6 +681,9 @@ intersect(Face_handle f, int i,
     case 2 : vi = vcc; break;
     case 3 : vi = vdd; break; 
     }
+    if(vi == vaa || vi == vbb) {
+      remove_constrained_edge(f, i);
+    }
   }
   else{ //intersection computed
     remove_constrained_edge(f, i);
@@ -1166,6 +1169,7 @@ intersection(const Gt& gt,
 {
   return compute_intersection(gt,pa,pb,pc,pd,pi);
 }
+
 
 template<class Gt>
 bool

.gitignore
@@ -1054,6 +1054,7 @@ Triangulation_2/test/Triangulation_2/T??.triangulation
 Triangulation_2/test/Triangulation_2/cgal_test_with_cmake
 Triangulation_2/test/Triangulation_2/file_tds*
 Triangulation_2/test/Triangulation_2/makefile
+Triangulation_2/test/Triangulation_2/test_cdt_degenerate_case
 Triangulation_2/test/Triangulation_2/test_const_del_triangulation_2
 Triangulation_2/test/Triangulation_2/test_const_triang_plus_2
 Triangulation_2/test/Triangulation_2/test_constrained_triangulation_2

Triangulation_2/test/Triangulation_2/test_cdt_degenerate_case.cpp
@@ -1,0 +1,29 @@
+#include <CGAL/basic.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <iostream>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel EPIC;
+typedef EPIC::Point_2 Point_2;
+typedef CGAL::Triangulation_vertex_base_2<EPIC> Vb;
+typedef CGAL::Constrained_triangulation_face_base_2<EPIC> Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb, Fb> TDS;
+typedef CGAL::Exact_predicates_tag Itag;
+typedef CGAL::Constrained_Delaunay_triangulation_2<EPIC, TDS, Itag> CDT;
+
+int main()
+{
+  CDT cdt;
+  cdt.insert_constraint(Point_2(  48.0923419883269,   299.7232779774145  ), 
+                        Point_2(  66.05373710316852,  434.231770798343   ));
+  cdt.insert_constraint(Point_2(  22.476834473530154, 110.79888079041085 ), 
+                        Point_2(  36.24523901070941,  304.88274418524736 ));
+  cdt.insert_constraint(Point_2(  23.319798016622762, 122.68156630438044 ), 
+                        Point_2(  36.24523901070941,  304.88274418524736 ));
+  cdt.insert_constraint(Point_2( 193.08640258787054,  291.60426613216123 ), 
+                        Point_2(-106.13354405627629,  310.30717824826723 ));
+  // The insertion of the last constraint can lead to an infinite loop,
+  // that actually ends with a segfault once the stack overflows.
+  std::cout << cdt.number_of_vertices() << std::endl;
+  return 0;
+}

commit 73e5f1a9e339d4c59caf8b3a813421627e59d770

Fix a bug in <CGAL/Constrained_triangulation_2.h>
The function parameter was locally shadowed by a local `int i`
variable!

Then the edge `(f, i)` in the call `remove_constrained_edge(f, i)` was
the wrong one!

The bug was introduced by ecfd82e287378977a325ae34e21087a2075a05cd.

Triangulation_2/include/CGAL/Constrained_triangulation_2.h
@@ -1054,8 +1054,8 @@ intersect(Face_handle f, int i,
 
   Vertex_handle vi;
   if ( !ok) {  //intersection detected but not computed
-    int i = limit_intersection(geom_traits(), pa, pb, pc, pd, itag);
-    switch(i){
+    int int_index = limit_intersection(geom_traits(), pa, pb, pc, pd, itag);
+    switch(int_index){
     case 0 : vi = vaa; break;
     case 1 : vi = vbb; break;
     case 2 : vi = vcc; break;